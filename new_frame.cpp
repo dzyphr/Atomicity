799d11de268e0f1c9d7a55006cd0e8a9a5783d0c
justaguy12345 <ryanberk135@gmail.com>
2023-02-04 01:04:06 -0600

    fixed new_frame not copying .env into new folder with new implementation, started modularizing new_frame.cpp

--- new_frame.cpp
+++ new_frame.cpp
@@ -5,6 +5,12 @@ void standardOperations(string arg1);
 void extraArgCheck(int argc, char* argv[]);
 void arg2IsMultiFile(int argc, char* argv[]);
 void arg2IsConstructorArgs(int argc, char* argv[]);
+void arg2IsCAargcIsGorEto4(int argc, char* argv[]);
+void arg2IsMargcIsGorEto4(int argc, char* argv[]);
+void echoConstructorArgsTrue(string newFrameName);
+void echoMultiFileFalse(string newFrameName);
+void echoConstructorArgsFalse(string newFrameName);
+void echoMultiFileTrue(string newFrameName);
 int main(int argc, char* argv[])
 {
 	if (argc < 2)
@@ -24,8 +30,8 @@ int main(int argc, char* argv[])
 
 void standardOperations(string arg1)
 {
-	string newFrameName = arg1;
-	string command = "mkdir " + newFrameName + " && cp -r basic_framework/* " + newFrameName + "/";
+	string newFrameName = arg1;		//VVV  make contract dir && copy basic framework to there && copy hidden .env file to there VVV
+	string command = "mkdir " + newFrameName + " && cp -r basic_framework/* " + newFrameName + "/ && cp -r basic_framework/. " + newFrameName;
 	system(command.c_str());
 	string newContract = "echo \'// SPDX-License-Identifier: GPL-3.0-only\npragma solidity >=0.8.0 <0.9.0;\n\ncontract "+ newFrameName +"\n{\n\n}\' > " + newFrameName + "/contracts/" + newFrameName + ".sol";
 	system(newContract.c_str());
@@ -58,154 +64,144 @@ void extraArgCheck(int argc, char* argv[])
 	}
 	else
 	{
-		string echoMultiFileFalse = "echo \'MultiFile=\"False\"\' >> " + newFrameName + "/.env";
-		string echoContructorArgsFalse = "echo \'ConstructorArgs=\"False\"\' >> " + newFrameName + "/.env";
-		system(echoMultiFileFalse.c_str());
-		system(echoContructorArgsFalse.c_str());
+		echoMultiFileFalse(newFrameName);
+		echoConstructorArgsFalse(newFrameName);
 	}
 }
 
 void arg2IsMultiFile(int argc, char* argv[])
 {
 	string newFrameName = argv[1];
-	string echoFlatten = 
-		"echo \'{\"inputFilePath\": \"../" + 
-		newFrameName  + "/contracts/" + 
-		newFrameName  + ".sol\",\"outputDir\": \"../" + 
-		newFrameName  + "/contracts/\"}\' > solidity-flattener/config.json";
-	system(echoFlatten.c_str());
-	string echoMultiFileTrue = "echo \'MultiFile=\"True\"\' >> " + newFrameName + "/.env";
-	system(echoMultiFileTrue.c_str());
+	echoMultiFileTrue(newFrameName);
 	if (argc >= 4)
 	{
-		if (argc >= 5)
-		{
-			string cacheck = argv[3];
-			if (cacheck == "-CA")
-			{
-				string echoContructorArgsTrue = "echo \'ConstructorArgs=\"True\"\' >> " + newFrameName + "/.env";
-				system(echoContructorArgsTrue.c_str());
-				string str_dec = argv[4];
-				string::size_type size;
-				int constructorArgCount = std::stoi(str_dec, &size);
-				vector<string> argumentVector;
-				int argumentIndex = 5;
-				string pyFmtArgArray = "constructorParamVals = [";
-				if (argc >= argumentIndex + constructorArgCount)
-				{
-					for (int i = 0; i < constructorArgCount; i++)
-					{
-						argumentVector.push_back(argv[argumentIndex + i]);
-					}
-					int i =0;
-					for (auto const& value : argumentVector)
-					{
-					//	cout << value << '\n';
-						pyFmtArgArray = pyFmtArgArray + value;
-					       	if (i != argumentVector.size()-1)
-						{
-							pyFmtArgArray = pyFmtArgArray + ',';
-						}	
-						i = i + 1;
-					}
-					pyFmtArgArray = pyFmtArgArray + ']';
-					string pyEchoArgArray = "echo \'" + pyFmtArgArray + "\' | cat - " + newFrameName  + "/py/deploy.py > pyFmtArgsTEMP && mv pyFmtArgsTEMP " + newFrameName + "/py/deploy.py";
-		                        system(pyEchoArgArray.c_str());
-
-					//cout << pyFmtArgArray << '\n';
-				}
-				else
-				{
-					cout << "Number of constructor args exceeds provided values for those arguments!\n";
-					exit(1);
-				}
-
-			}
-			else
-			{
-				cout << "unrecognized command: " << argv[3] << '\n';
-				exit(1);
-			}
-		}
-		else
-		{
-			cout << "-CA flag used without providing count of constructor args, provide them as next arg!\n";
-			exit(1);
-		}
+		arg2IsMargcIsGorEto4(argc, argv);
 	}
 	else
 	{
-		string echoContructorArgsFalse = "echo \'ConstructorArgs=\"False\"\' >> " + newFrameName + "/.env";
-		system(echoContructorArgsFalse.c_str());
-
+		echoConstructorArgsFalse(newFrameName);
 	}
 }
 
 void arg2IsConstructorArgs(int argc, char* argv[])
 {
-	string newFrameName = argv[1];
-	string echoContructorArgsTrue = "echo \'ConstructorArgs=\"True\"\' >> " + newFrameName + "/.env";
-	system(echoContructorArgsTrue.c_str());
+	echoConstructorArgsTrue(argv[1]);
 	if (argc >= 4)
 	{
-		string str_dec = argv[3];
-		string::size_type size;
-		int constructorArgCount = std::stoi(str_dec, &size);
-		vector<string> argumentVector;
-		int argumentIndex = 4;
-		string pyFmtArgArray = "constructorParamVals = [";
-		if (argc >= argumentIndex + constructorArgCount)
-		{
-			for (int i = 0; i < constructorArgCount; i++)
-			{
-				argumentVector.push_back(argv[argumentIndex + i]);
-			}
-			int i = 0;
-			for (auto const& value : argumentVector)
-			{
-				//cout << value << '\n';
-				pyFmtArgArray = pyFmtArgArray + value;
-				if (i != argumentVector.size()-1)
-				{
-					pyFmtArgArray = pyFmtArgArray + ',';
-				}
-				i = i + 1;
+		arg2IsCAargcIsGorEto4(argc, argv);
+	}
+	else
+	{
+		cout << "-CA flag used without providing count of constructor args, provide them as next arg!\n";
+		exit(1);
+	}
+}
 
+void arg2IsCAargcIsGorEto4(int argc, char* argv[])
+{
+	string newFrameName = argv[1];
+	string str_dec = argv[3];
+	string::size_type size;
+	int constructorArgCount = std::stoi(str_dec, &size);
+	vector<string> argumentVector;
+	int argumentIndex = 4;
+	string pyFmtArgArray = "constructorParamVals = [";
+	if (argc >= argumentIndex + constructorArgCount)
+	{
+		for (int i = 0; i < constructorArgCount; i++)
+		{
+			argumentVector.push_back(argv[argumentIndex + i]);
+		}
+		int i = 0;
+		for (auto const& value : argumentVector)
+		{
+			//cout << value << '\n';
+			pyFmtArgArray = pyFmtArgArray + value;
+			if (i != argumentVector.size()-1)
+			{
+				pyFmtArgArray = pyFmtArgArray + ',';
 			}
-			pyFmtArgArray = pyFmtArgArray + ']';
-			string pyEchoArgArray = "echo \'" + pyFmtArgArray + "\' | cat - " + newFrameName  + "/py/deploy.py > pyFmtArgsTEMP && mv pyFmtArgsTEMP " + newFrameName + "/py/deploy.py";
-			system(pyEchoArgArray.c_str());
-			//cout << pyFmtArgArray << '\n';
+			i = i + 1;
+
+		}
+		pyFmtArgArray = pyFmtArgArray + ']';
+		string pyEchoArgArray = "echo \'" + pyFmtArgArray + "\' | cat - " + newFrameName  + "/py/deploy.py > pyFmtArgsTEMP && mv pyFmtArgsTEMP " + newFrameName + "/py/deploy.py";
+		system(pyEchoArgArray.c_str());
+		//cout << pyFmtArgArray << '\n';
+	}
+	else
+	{
+		cout << "Number of constructor args exceeds provided values for those arguments!\n";
+		exit(1);
+	}
+	if (argc > argumentIndex + constructorArgCount)
+	{
+		string currentArg = argv[argumentIndex + constructorArgCount];
+		if (currentArg == "-M")
+		{
+			echoMultiFileTrue(newFrameName);
 		}
 		else
 		{
-			cout << "Number of constructor args exceeds provided values for those arguments!\n";
+			cout << "unrecognized command: " << argv[argumentIndex + constructorArgCount] << '\n';
 			exit(1);
 		}
-		if (argc > argumentIndex + constructorArgCount)
+	}
+	else
+	{
+		echoMultiFileFalse(newFrameName);
+	}
+}
+
+void arg2IsMargcIsGorEto4(int argc, char* argv[])
+{
+	string newFrameName = argv[1];
+	if (argc >= 5)
+	{
+		string cacheck = argv[3];
+		if (cacheck == "-CA")
 		{
-			string currentArg = argv[argumentIndex + constructorArgCount];
-			if (currentArg == "-M")
+			echoConstructorArgsTrue(newFrameName);
+			string str_dec = argv[4];
+			string::size_type size;
+			int constructorArgCount = std::stoi(str_dec, &size);
+			vector<string> argumentVector;
+			int argumentIndex = 5;
+			string pyFmtArgArray = "constructorParamVals = [";
+			if (argc >= argumentIndex + constructorArgCount)
 			{
-				string echoMultiFileTrue = "echo \'MultiFile=\"True\"\' >> " + newFrameName + "/.env";
-				system(echoMultiFileTrue.c_str());
-				string echoFlatten = 
-					"echo \'{\"inputFilePath\": \"../" + 
-					newFrameName  + "/contracts/" + 
-					newFrameName  + ".sol\",\"outputDir\": \"../" + 
-					newFrameName  + "/contracts/\"}\' > solidity-flattener/config.json";
-				system(echoFlatten.c_str());
+				for (int i = 0; i < constructorArgCount; i++)
+				{
+					argumentVector.push_back(argv[argumentIndex + i]);
+				}
+				int i =0;
+				for (auto const& value : argumentVector)
+				{
+				//      cout << value << '\n';
+					pyFmtArgArray = pyFmtArgArray + value;
+					if (i != argumentVector.size()-1)
+					{
+						pyFmtArgArray = pyFmtArgArray + ',';
+					}       
+					i = i + 1;
+				}
+				pyFmtArgArray = pyFmtArgArray + ']';
+				string pyEchoArgArray = "echo \'" + pyFmtArgArray + "\' | cat - " + newFrameName  + "/py/deploy.py > pyFmtArgsTEMP && mv pyFmtArgsTEMP " + newFrameName + "/py/deploy.py";
+				system(pyEchoArgArray.c_str());
+
+				//cout << pyFmtArgArray << '\n';
 			}
 			else
 			{
-				cout << "unrecognized command: " << argv[argumentIndex + constructorArgCount] << '\n';
+				cout << "Number of constructor args exceeds provided values for those arguments!\n";
 				exit(1);
 			}
+
 		}
 		else
 		{
-			string echoMultiFileFalse = "echo \'MultiFile=\"False\"\' >> " + newFrameName + "/.env";
-			system(echoMultiFileFalse.c_str());
+			cout << "unrecognized command: " << argv[3] << '\n';
+			exit(1);
 		}
 	}
 	else
@@ -214,3 +210,34 @@ void arg2IsConstructorArgs(int argc, char* argv[])
 		exit(1);
 	}
 }
+
+void echoConstructorArgsTrue(string newFrameName)
+{
+        string echoContructorArgsTrue = "echo \'ConstructorArgs=\"True\"\' >> " + newFrameName + "/.env";
+        system(echoContructorArgsTrue.c_str());
+}
+
+void echoConstructorArgsFalse(string newFrameName)
+{
+        string echoContructorArgsFalse = "echo \'ConstructorArgs=\"False\"\' >> " + newFrameName + "/.env";
+        system(echoContructorArgsFalse.c_str());
+}
+
+void echoMultiFileTrue(string newFrameName)
+{
+        string echoMultiFileTrue = "echo \'MultiFile=\"True\"\' >> " + newFrameName + "/.env";
+        system(echoMultiFileTrue.c_str());
+        string echoFlatten =
+                "echo \'{\"inputFilePath\": \"../" +
+                newFrameName  + "/contracts/" +
+                newFrameName  + ".sol\",\"outputDir\": \"../" +
+                newFrameName  + "/contracts/\"}\' > solidity-flattener/config.json";
+        system(echoFlatten.c_str());
+}
+
+void echoMultiFileFalse(string newFrameName)
+{
+        string echoMultiFileFalse = "echo \'MultiFile=\"False\"\' >> " + newFrameName + "/.env";
+        system(echoMultiFileFalse.c_str());
+}
+
